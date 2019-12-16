594626d588217a86563b4f4fc309ef0c34fbb8c7
Kejal Bhuva <Kejal.Bhuva@codeimmersives.com>
2019-12-15 22:43:32 -0500

    display tin and display dough functions created

--- main.js
+++ main.js
@@ -4,34 +4,91 @@ const doughList = [];
 document.querySelector('#add-tin').addEventListener('click', function () {
     const biscuit_inp = document.querySelector('#biscuit_input');
     const biscuit = biscuit_inp.value;
+    biscuit_inp.value = "";
+    biscuit_inp.focus();
     biscuit.trim();
     if (biscuit.length !== 0) {
         tin.push(biscuit);
     }
 
-    document.querySelector('#tin').innerHTML = '';
-
-    for (let i = 0; i < tin.length; i++) {
-        let li = document.createElement('li');
-        li.innerText = tin[i];
-        document.querySelector('#tin').appendChild(li);
-    }
+    displayTin();
 });
 
 
 document.querySelector('#prep-dough').addEventListener('click', function () {
     const biscuit_inp = document.querySelector('#biscuit_input');
     const biscuit = biscuit_inp.value;
+    biscuit_inp.value = "";
+    biscuit_inp.focus();
     biscuit.trim();
     if (biscuit.length !== 0) {
-    doughList.push(biscuit);
+        doughList.push(biscuit);
+    }
+
+    displayDough();
+});
+
+function displayTin(){
+    document.querySelector('#tin ul').innerHTML = '';
+
+    for (let i = 0; i < tin.length; i++) {
+        let li = document.createElement('li');
+        li.innerText = tin[i];
+        let editButton = document.createElement("button");
+        let xButton = document.createElement("button");
+        editButton.innerHTML = "&#x270f;";
+        editButton.setAttribute("data-index", i);
+        xButton.innerHTML = "&times;";
+        xButton.setAttribute("data-index", i);
+        li.appendChild(editButton);
+        li.appendChild(xButton);
+        document.querySelector('#tin ul').appendChild(li);
+    }
+
+    document.querySelectorAll("#tin ul button:first-child").forEach(function(button){
+        button.addEventListener("click", editInTin);
+    });
+
+    document.querySelectorAll("#tin ul button:last-child").forEach(function(button){
+        button.addEventListener("click", removeFromTin);
+    });
+    
 }
 
-    document.querySelector('#dough').innerHTML = '';
+function displayDough(){
+    document.querySelector('#factory ul').innerHTML = '';
 
     for (let i = 0; i < doughList.length; i++) {
         let li = document.createElement('li');
-        li.innerText = doughList[i];
-        document.querySelector('#dough').appendChild(li);
+        li.innerText = tin[i];
+        let editButton = document.createElement("button");
+        let xButton = document.createElement("button");
+        editButton.innerHTML = "&#x270f;";
+        editButton.setAttribute("data-index", i);
+        xButton.innerHTML = "&times;";
+        xButton.setAttribute("data-index", i);
+        li.appendChild(editButton);
+        li.appendChild(xButton);
+        document.querySelector('#tin ul').appendChild(li);
     }
-});
+    
+    document.querySelectorAll("#factory ul button:first-child").forEach(function(button){
+        button.addEventListener("click", editInDough);
+    });
+    
+    document.querySelectorAll("#factory ul button:last-child").forEach(function(button){
+        button.addEventListener("click", removeFromDough);
+    });
+}
+
+function editInTin(){
+}
+
+function removeFromTin(){
+}
+
+function editInDough(){
+}
+
+function removeFromDough(){
+}
